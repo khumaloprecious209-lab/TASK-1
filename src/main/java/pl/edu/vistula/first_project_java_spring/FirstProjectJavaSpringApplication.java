63f48c9f77a1d28dea3489d4e2b873d5ba25544a
khumaloprecious209-lab <khumaloprecious209@gmail.com>
2026-05-11 09:20:49 +0200

    first spring project

--- src/main/java/pl/edu/vistula/first_project_java_spring/FirstProjectJavaSpringApplication.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/FirstProjectJavaSpringApplication.java
@@ -10,4 +10,6 @@ public class FirstProjectJavaSpringApplication {
 		SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
 	}
 
+
+
 }
--- src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -17,4 +17,7 @@ public class HelloController {
         model.addAttribute("name", name);
         return "greeting";
     }
+
 }
+
+
